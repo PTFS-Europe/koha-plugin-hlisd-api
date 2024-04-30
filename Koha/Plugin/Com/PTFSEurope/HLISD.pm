e22438311bd1c801821db0ed53bf2af88d08cb8f
Pedro Amorim <pedro.amorim@ptfs-europe.com>
2024-04-30 10:22:14 +0000

    First cron commit. Logic in module

--- Koha/Plugin/Com/PTFSEurope/HLISD.pm
+++ Koha/Plugin/Com/PTFSEurope/HLISD.pm
@@ -7,12 +7,13 @@ use warnings;
 use base            qw(Koha::Plugins::Base);
 use Koha::DateUtils qw( dt_from_string );
 
-use JSON qw( encode_json decode_json );
+use JSON           qw( encode_json decode_json );
 use File::Basename qw( dirname );
 use C4::Installer;
-use Cwd            qw(abs_path);
+use Cwd qw(abs_path);
 use CGI;
 use JSON qw( decode_json );
+use C4::Context;
 
 use Koha::Plugin::Com::PTFSEurope::HLISD::Lib::API;
 
@@ -38,7 +39,6 @@ Required I<Koha::Plugin> method
 sub new {
     my ( $class, $args ) = @_;
 
-
     ## We need to add our metadata here so our base class can access it
     $args->{'metadata'} = $metadata;
     $args->{'metadata'}->{'class'} = $class;
@@ -50,7 +50,7 @@ sub new {
 
     $self->{config} = decode_json( $self->retrieve_data('hlisd_config') || '{}' );
 
-    my $api = Koha::Plugin::Com::PTFSEurope::HLISD::Lib::API->new($self->{config});
+    my $api = Koha::Plugin::Com::PTFSEurope::HLISD::Lib::API->new( $self->{config} );
     $self->{_api} = $api;
 
     return $self;
@@ -98,6 +98,143 @@ sub upgrade {
 
 sub uninstall() {
     return 1;
+}
+
+=head3 harvest_hlisd
+
+TODO: Implement the code to actually do the harvesting, see the TODO comment in the code
+
+=cut
+
+sub harvest_hlisd {
+    my ( $self, $args ) = @_;
+
+    $self->_config_check();
+
+    my $patrons = $self->_get_patrons();
+
+    debug_msg(
+        $args->{debug},
+        sprintf( "Found %d %s patrons", $patrons->count(), C4::Context->preference('ILLPartnerCode') ));
+
+    while ( my $patron = $patrons->next ) {
+
+        #TODO: Get library id from patron attribute type configured
+
+        #TODO: call LibraryDetails API
+
+        # my $plugin = Koha::Plugin::Com::PTFSEurope::HLISD->new();
+        # my $api    = $plugin->{_api};
+        # my $res    = $api->LibraryDetails(3610);
+
+        # if ( $res->is_success ) {
+        #     debug_msg(
+        #         $args->{debug},
+        #         sprintf( "Harvesting data for patron #%s - %s", $patron->borrowernumber, $patron->surname )
+        #     );
+
+        #     my $data = decode_json( $res->decoded_content() );
+
+        #     if ( $data->{status} eq 'success' && $data->{data} ) {
+        #         $self->store_data(
+        #             {
+        #                 patron_id  => $patron->borrowernumber,
+        #                 hlisd_data => encode_json( $data->{data} ),
+        #             }
+        #         );
+        #     } else {
+        #         debug_msg(
+        #             $args->{debug},
+        #             sprintf(
+        #                 "HL-ISD API returned error for patron #%s - %s", $patron->borrowernumber, $patron->surname
+        #             )
+        #         );
+        #     }
+
+        # } else {
+        #     debug_msg(
+        #         $args->{debug},
+        #         sprintf( "HL-ISD API request failed for patron #%s - %s", $patron->borrowernumber, $patron->surname )
+        #     );
+        # }
+    }
+}
+
+=head3 debug_msg
+
+ Prints a debug message to STDERR if debug mode is enabled.
+
+ Parameters:
+ - $msg: The message to be printed. If it is a hash reference, it will be converted to a string using Data::Dumper.
+
+ Returns: None.
+
+=cut
+
+sub debug_msg {
+    my ( $debug, $msg ) = @_;
+
+    if ( !$debug ) {
+        return;
+    }
+
+    if ( ref $msg eq 'HASH' ) {
+        use Data::Dumper;
+        $msg = Dumper $msg;
+    }
+    print STDERR "$msg\n";
+}
+
+=head3 _get_patrons
+
+ Retrieves patrons from the database based on the partner code set in the system preferences.
+ 
+ Parameters:
+ - $self: The object instance of the class.
+ 
+ Returns:
+ - Koha::Patrons: A collection of patrons.
+
+=cut
+
+sub _get_patrons {
+    my ($self) = @_;
+
+    my $partner_code = C4::Context->preference('ILLPartnerCode');
+    unless ($partner_code) {
+        die "ERROR: No ILL partner code set. Please set the ILLPartnerCode system preference.";
+    }
+
+    my $patrons = Koha::Patrons->search( { categorycode => $partner_code } );
+    unless ( scalar @{ $patrons->as_list() } ) {
+        die "ERROR: No ILL partner patrons found.";
+    }
+
+    return $patrons;
+}
+
+=head3 _config_check
+
+ Checks if the HL-ISD API credentials are set in the configuration.
+ If not, it prints a debug message and returns.
+
+ Parameters:
+ - $self: The object instance.
+
+ Returns: None.
+
+=cut
+
+sub _config_check {
+    my ($self) = @_;
+
+    my $config = $self->{config};
+
+    unless ( $config->{email} && $config->{password} ) {
+        die "HL-ISD API credentials not set, skipping harvest";
+    }
+
+    
 }
 
 1;
