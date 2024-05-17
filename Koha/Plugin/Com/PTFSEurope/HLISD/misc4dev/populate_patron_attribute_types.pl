31c9a51c7ae13ae62e494e978617690fad80054d
Pedro Amorim <pedro.amorim@ptfs-europe.com>
2024-05-17 12:21:19 +0000

    Use ill_partner_category in populate_patron_attribute_types

--- Koha/Plugin/Com/PTFSEurope/HLISD/misc4dev/populate_patron_attribute_types.pl
+++ Koha/Plugin/Com/PTFSEurope/HLISD/misc4dev/populate_patron_attribute_types.pl
@@ -1,7 +1,10 @@
 
 use Try::Tiny;
+use C4::Context;
 
 use Koha::Patron::Attribute::Type;
+
+my $ill_partner_category = C4::Context->preference('ILLPartnerCode') || 'IL';
 
 try {
     Koha::Patron::Attribute::Type->new(
