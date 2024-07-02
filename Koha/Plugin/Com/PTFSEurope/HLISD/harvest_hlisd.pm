244fda0534e37c83620f5ca14d6cad25f021973f
Pedro Amorim <pedro.amorim@ptfs-europe.com>
2024-07-02 09:46:19 +0000

    Add 'mode' option

--- Koha/Plugin/Com/PTFSEurope/HLISD/harvest_hlisd.pm
+++ Koha/Plugin/Com/PTFSEurope/HLISD/harvest_hlisd.pm
@@ -28,9 +28,11 @@ use Koha::Plugin::Com::PTFSEurope::HLISD;
 # Command line option values
 my $get_help   = 0;
 my $debug      = 0;
+my $mode;
 
 my $options = GetOptions(
     'h|help'       => \$get_help,
+    'mode|m=s'     => \$mode,
     'debug'        => \$debug
 );
 
@@ -39,9 +41,13 @@ if ($get_help) {
     exit 1;
 }
 
+die "A mode needs to be specified with the -m or --mode option ('library' or 'patron')" unless $mode;
+die "Invalid mode supplied ('library' or 'patron' is expected)" unless $mode eq 'patron' || $mode eq 'library';
+
 Koha::Plugin::Com::PTFSEurope::HLISD->new()->harvest_hlisd(
     {
-        debug   => $debug
+        debug   => $debug,
+        mode    => $mode
     }
 );
 
@@ -52,9 +58,10 @@ $0: Run a HLISD harvest
 Parameters:
     --help or -h                         get help
     --debug                              print additional debugging info during run
+    --mode or -m                         specificies mode: 'patron' or 'library'
 
 Usage example:
-./misc/cronjobs/harvest_hlisd.pl --debug
+./misc/cronjobs/harvest_hlisd.pl --mode patron --debug
 
 HELP
 }
